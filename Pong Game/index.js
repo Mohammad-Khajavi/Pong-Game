00d0d2de3eb302c213b8f33632144f3b0c3ae4f3
mohammad <151404726+Mohammad-Khajavi@users.noreply.github.com>
2024-10-31 00:11:50 -0400

    create the variables and constant as well the empty function for the game

---
+++ Pong Game/index.js
@@ -0,0 +1,49 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackGround = "foresetgreen";
+const paddleColor = "lightblue";
+const paddleColor2 = "red";
+const paddleBorderColor = "black";
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
+
+let paddle1 = {
+  width: 25,
+  height: 100,
+  x: 0,
+  y: 0,
+};
+
+let paddle2 = {
+  width: 25,
+  height: 100,
+  x: gameWidth - 25,
+  y: gameHeight - 100,
+};
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
+function gameStart() {}
+function bextTick() {}
+function clearBoard() {}
+function drawPaddle() {}
+function createBall() {}
+function moveBall() {}
+function checkCollision() {}
+function changeDirection() {}
+function updateScore() {}
